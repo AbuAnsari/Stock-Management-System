a3a5736d21998e1055e8379babb781f4d8c54e4a
Abu Aiub Ansari <abuaiubansari@outlook.com>
2018-11-05 13:43:26 +0600

    Add Save to Category Manager

---
+++ StockManagementApplication/StockManagementApplication/BLL/CategoryManager.cs
@@ -0,0 +1,16 @@
+﻿using StockManagementApplication.DAL;
+using StockManagementApplication.Models;
+
+namespace StockManagementApplication.BLL
+{
+    public class CategoryManager
+    {
+        readonly CategoryRepository _categoryRepository = new CategoryRepository();
+
+        public bool Save(Category category)
+        {
+            var save = _categoryRepository.Save(category);
+            return save;
+        }
+    }
+}
