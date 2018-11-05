07e1e34d2f21ff3f73ac8e0aced0c06782ea9036
Abu Aiub Ansari <abuaiubansari@outlook.com>
2018-11-05 13:19:02 +0600

    Add Category Repository

---
+++ StockManagementApplication/StockManagementApplication/DAL/CategoryRepository.cs
@@ -0,0 +1,25 @@
+﻿using StockManagementApplication.Models;
+using System;
+
+namespace StockManagementApplication.DAL
+{
+    public class CategoryRepository
+    {
+        private readonly string _connectionString = GenericRepository.ConnectionString();
+        readonly GenericRepository _genericRepository = new GenericRepository();
+
+        public bool Save(Category category)
+        {
+            try
+            {
+                var query = "";
+                var rowAffected = _genericRepository.ExecuteNonQuery(query, _connectionString);
+                return rowAffected > 0;
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+    }
+}
