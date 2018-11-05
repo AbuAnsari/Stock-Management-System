593714df4fec832e3fafa0d8b86778faeec39b4a
Abu Aiub Ansari <abuaiubansari@outlook.com>
2018-11-05 12:46:26 +0600

    Modified Category Model

--- StockManagementApplication/StockManagementApplication/Models/Category.cs
+++ StockManagementApplication/StockManagementApplication/Models/Category.cs
@@ -1,9 +1,18 @@
 ﻿
+using System;
+
 namespace StockManagementApplication.Models
 {
     public class Category
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string CreateBy { get; set; }
+        public DateTime CreateDate { get; set; }
+        public string UpdateBy { get; set; }
+        public DateTime? UpdateDate { get; set; }
+        public string DeleteBy { get; set; }
+        public DateTime? DeleteDate { get; set; }
+
     }
 }
